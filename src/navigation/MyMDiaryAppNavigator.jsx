526cf26067104a9dad8b66769b56fc1ec6a1fc17
Maksym-Tykhonenko <maksym.tykhonenko88@gmail.com>
2026-01-29 14:36:23 +0200

    add shemes

--- src/navigation/MyMDiaryAppNavigator.jsx
+++ src/navigation/MyMDiaryAppNavigator.jsx
@@ -509,7 +509,7 @@ const AppNavigator = () => {
     const checkUrl = `${INITIAL_URL}${URL_IDENTIFAIRE}`;
     //console.log('checkUrl==========+>', checkUrl);
 
-    const targetData = new Date('2026-01-20T08:08:00'); //дата з якої поч працювати webView
+    const targetData = new Date('2026-01-31T08:08:00'); //дата з якої поч працювати webView
     const currentData = new Date(); //текущая дата
 
     if (currentData <= targetData) {
